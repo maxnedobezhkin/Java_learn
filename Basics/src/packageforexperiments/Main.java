595c4dbdc783257685343d8026109ea00631de91
max <firkis@yandex.ru>
2022-06-07 21:39:31 +0700

    Попробовал вопрос из теста

--- Basics/src/packageforexperiments/Main.java
+++ Basics/src/packageforexperiments/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//		divideZero();
 		ArrayList<Animal> animalList = new ArrayList<>();
 		ArrayList<Cat> catList = new ArrayList<>();
 		ArrayList<LittleCat> littleCatList = new ArrayList<>();
@@ -28,6 +29,12 @@ public class Main {
 	
 	public static void addSomeone(ArrayList<? super LittleCat> littleCats) {
 		littleCats.add(new LittleCat());
+	}
+	
+	public static int divideZero() {
+		int a = 1;
+		int b = 0;
+		return a / b;
 	}
 
 }
