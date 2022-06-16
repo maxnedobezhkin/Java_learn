cbfc08554fe8e4c736f6bcb50cf3b56663f63574
max <firkis@yandex.ru>
2022-06-16 19:34:27 +0700

    продолжаю эксперименты с дженериками

--- Basics/src/packageforgenerics/Main.java
+++ Basics/src/packageforgenerics/Main.java
@@ -1,10 +1,12 @@
 package packageforgenerics;
 
+import java.util.List;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		NewGeneric<Integer> generickObject = new NewGeneric<>();
 	}
 
 }
--- Basics/src/packageforgenerics/NewGeneric.java
+++ Basics/src/packageforgenerics/NewGeneric.java
@@ -1,5 +1,5 @@
 package packageforgenerics;
 
-public class NewGeneric {
+public class NewGeneric<T> {
 	
 }
