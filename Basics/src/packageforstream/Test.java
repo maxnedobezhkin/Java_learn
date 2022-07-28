f8de725b3d113816508e0022ee20fd28837ef302
max <firkis@yandex.ru>
2022-07-28 23:07:20 +0700

    Попробовал создать поток

--- Basics/src/packageforstream/Test.java
+++ Basics/src/packageforstream/Test.java
@@ -1,10 +1,15 @@
 package packageforstream;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Stream;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Collection<String> collection = Arrays.asList("f5", "b6", "z7");
+		Stream<String> collectionS = collection.stream();
 	}
 
 }
