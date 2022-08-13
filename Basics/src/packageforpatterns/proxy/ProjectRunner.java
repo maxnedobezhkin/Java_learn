9aaf296656b4ca97ac0c33ee0895e4de9097a696
max <firkis@yandex.ru>
2022-08-13 21:22:41 +0700

    Продолжаю работу с паттерном Прокси

--- Basics/src/packageforpatterns/proxy/ProjectRunner.java
+++ Basics/src/packageforpatterns/proxy/ProjectRunner.java
@@ -2,6 +2,7 @@ package packageforpatterns.proxy;
 
 public class ProjectRunner {
 	public static void main(String[] args) {
-		
+		Project project = new RealProject("github.com/maxnedobezhkin");
+		project.run();
 	}
 }
--- Basics/src/packageforpatterns/proxy/RealProject.java
+++ Basics/src/packageforpatterns/proxy/RealProject.java
@@ -1,5 +1,22 @@
 package packageforpatterns.proxy;
 
-public class RealProject {
+public class RealProject implements Project {
+	
+	private String url;
+	
+	public RealProject(String url) {
+		this.url = url;
+		load();
+	}
+	
+	public void load() {
+		System.out.println("Loading project from " + url);
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Running project");
+	}
 
 }
