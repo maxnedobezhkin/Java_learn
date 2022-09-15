5d37eac5230e68d7c27ad0d125ab97492cb917f5
max <firkis@yandex.ru>
2022-09-15 11:46:11 +0700

    Вернулся к практике со Stream

--- Basics/src/packageforstream/Test.java
+++ Basics/src/packageforstream/Test.java
@@ -2,14 +2,23 @@ package packageforstream;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Collection<String> collection = Arrays.asList("f5", "b6", "z7");
+		List<String> collection = Arrays.asList("f5", "b6", "z7", "f1");
 		Stream<String> collectionS = collection.stream();
+		List<String> sorted = collection.stream()
+				.filter(collectionElement -> collectionElement.contains("f"))
+				.sorted()
+				.collect(Collectors.toList());
+		sorted.forEach(System.out::println);
 	}
+	
 
 }
